0fbb507bf7d40b7095d9e5934fc67d8f55881d8c
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-19 19:57:02 +0530

    Home page init

--- frontend/src/Screens/List/List.jsx
+++ frontend/src/Screens/List/List.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
 import Navbar from "../../Components/Navbar/Navbar";
+import "./List.css";
 
 const List = () => {
   return (
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listSearch">h1</div>
+        <div className="listResult"></div>
+      </div>
     </div>
   );
 };
