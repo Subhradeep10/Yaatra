6d6fc9ce0a08691e79a34fed7b8aa27b3e68c3f5
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-19 20:48:56 +0530

    Frontend done

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">Yaatra</span>
+        <span className="logo">
+          <Link to="/" className="logo-new">
+            Yaatra
+          </Link>
+        </span>
+
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
