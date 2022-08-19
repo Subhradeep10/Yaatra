132e160b2246e7c33ba485399770ed3767d64430
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-19 19:34:33 +0530

    Home page init

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
           <li className="fListItem">Terms & conditions</li>
         </ul>
       </div>
-      <div className="fText">Copyright © 2022 Lamabooking.</div>
+      <div className="fText">Copyright © 2022 Yaatra.</div>
     </div>
   );
 };
