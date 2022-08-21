f1470928cadb527000eae57df98f4435a0d66cc4
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-21 22:35:28 +0530

    Backend API init

--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+//CREATE
+router.post('/', (req, res) => {
+});
+//UPDATE
+//DELETE
+//GET
+//GET ALL
 
 module.exports = router;
