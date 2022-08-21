34a37caaeb22f6a355b23ea4328eca8f057ec20c
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-21 21:49:16 +0530

    Mongo Connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+        mongoose.connect(process.env.MONGO_DB)
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.log(err);
+    }
+};
+
 
 app.listen(8800, () => {
-    console.log('Server is running on port 3000');
+    connect();
+    console.log('Server is running on port 8800');
 });
