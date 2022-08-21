5c4d75acb648999991962d26f8691ced5c76f8ac
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-21 23:52:15 +0530

    Hotel API donet

--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -13,8 +13,40 @@ router.post('/', async (req, res) => {
     }
 });
 //UPDATE
+router.put('/:id', async (req, res) => {
+    try {
+        const updateHotel = await hotels.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+        res.status(200).json(updateHotel);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 //DELETE
+router.delete('/:id', async (req, res) => {
+    try {
+        await hotels.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: 'Hotel deleted' });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 //GET
+router.get('/:id', async (req, res) => {
+    try {
+        const Hotel = await hotels.findById(req.params.id);
+        res.status(200).json(Hotel);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 //GET ALL
+router.get('/', async (req, res) => {
+    try {
+        const Hotels = await hotels.find();
+        res.status(200).json(Hotels);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router;
