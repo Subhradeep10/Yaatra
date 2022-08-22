6355793faf13c914c8ee5206412a444d3df360ed
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-22 18:48:28 +0530

    auth done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const login = require('../controllers/auth');
 const register = require('../controllers/auth');
 const router = express.Router();
 
 router.post('/register', register);
-
+router.post('/login', login);
 
 module.exports = router;
