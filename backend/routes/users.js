6597b1c10392c3bdd8d290002926dd60bdcb354c
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-22 19:58:49 +0530

    User init

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const updateUser = require('../controllers/users');
+const deleteUser = require('../controllers/users');
+const getUser = require('../controllers/users');
+const getAllUsers = require('../controllers/users');
 
 
+//UPDATE
+router.put('/:id', updateUser);
+//DELETE
+router.delete('/:id', deleteUser);
+//GET
+router.get('/:id', getUser);
+//GET ALL
+router.get('/', getAllUsers);
 module.exports = router;
