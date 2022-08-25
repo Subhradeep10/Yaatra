e44ec6de16d0c22b339307f7345ae2956fe35744
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-25 23:09:46 +0530

    Auth init

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,15 +3,20 @@ const updateUser = require('../controllers/users');
 const deleteUser = require('../controllers/users');
 const getUser = require('../controllers/users');
 const getAllUsers = require('../controllers/users');
+const verifyAdmin = require('../utils/verifytoken');
 const verifyToken = require('../utils/verifytoken');
+const verifyUser = require('../utils/verifytoken');
 
 const router = express.Router();
 
 
-router.get('/checkauthentication', verifyToken, (req, res, next) => {
-    res.json({ message: 'You are authenticated' });
-})
+// router.get('/checkauthentication', verifyToken, (req, res, next) => {
+//     res.json({ message: 'You are authenticated' });
+// })
 
+// router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
+//     res.json({ message: 'Hello Admin, You are logged in' });
+// })
 
 //UPDATE
 router.put('/:id', updateUser);
