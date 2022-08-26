341def947e2bf9416f49cf066c716750b44fd1b1
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-26 16:42:38 +0530

    basic Backend completed

--- backend/controllers/room.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const Room = require('../models/rooms');
-const Hotel = require('../models/hotels');
-const createError = require('../utils/error');
-
--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -1,5 +1,27 @@
 const express = require('express');
+const createRoom = require('../controllers/room/createRoom');
+const getRoom = require('../controllers/room/getRoom');
+const updateRoom = require('../controllers/room/updateRoom');
+const deleteRoom = require('../controllers/room/deleteRoom');
+const getAllRooms = require('../controllers/room/getAllRooms');
+const updateRoomAvailability = require('../controllers/room/updateRoomAvailability');
+const verifyAdmin = require('../utils/verifytoken');
+
 const router = express.Router();
 
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//UPDATE
+router.put("/availability/:id", updateRoomAvailability);
+router.put("/:id", verifyAdmin, updateRoom);
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+//GET
+
+router.get("/:id", getRoom);
+//GET ALL
+
+router.get("/", getAllRooms);
 
 module.exports = router;
